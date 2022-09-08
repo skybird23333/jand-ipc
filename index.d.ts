a53ea7336d0927a35bcabf1c215e457c8ddf0fcb
2603003199a <58811224+skybird23333@users.noreply.github.com>
2022-09-08 19:02:30 +0800

    Initial Commit

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+export function connect(): Promise<void>;
+export function getProcessList(): Promise<ProcessInfo[]>;
+export function getRuntimeProcessList(): Promise<RuntimeProcessInfo[]>;
+export function getDaemonStatus(): Promise<DaemonStatus>;
+export function renameProcess(oldname: string, newname: string): Promise<void>;
+export function sendProcessStdinLine(processname: string, text: string): Promise<void>;
+export function exit(): Promise<void>;
+export function setEnabled(process: string, enabled: boolean): Promise<void>;
+export function setProcessProperty(process: string, property: string, data: string): Promise<void>;
+export function getProcessInfo(process: ProcessInfo): Promise<any>;
+export function stopProcess(process: string): Promise<boolean>;
+export function restartProcess(process: string): Promise<void>;
+export function newProcess(process: NewProcess): Promise<void>;
+export function saveConfig(): Promise<void>;
+export function getConfig(): Promise<any>;
+export function setConfig(option: string, value: string): Promise<void>;
+export function flushAllLogs(): Promise<void>;
+export type ProcessInfo = {
+    Name: string;
+    Filename: string;
+    Arguments: string[];
+    WorkingDirectory: string;
+    AutoRestart: boolean;
+    Enabled: boolean;
+};
+export type RuntimeProcessInfo = {
+    Name: string;
+    Filename: string;
+    Arguments: string[];
+    WorkingDirectory: string;
+    AutoRestart: boolean;
+    ProcessId: number;
+    ExitCode: number;
+    RestartCount: number;
+};
+export type NewProcess = {
+    Name: string;
+    Filename: string;
+    Arguments: string[];
+    WorkingDirectory: string;
+};
+export type SetPropertyIpcPacket = {
+    Process: string;
+    Property: string;
+    data: string;
+};
+export type DaemonStatus = {
+    Processes: number;
+    NotSaved: boolean;
+    Directory: string;
+    Version: string;
+};
---
+++ index.js
@@ -0,0 +1,282 @@
+//@ts-check
+const net = require('net')
+let socket
+
+var DEBUG = true
+
+/**
+ * @typedef {Object} ProcessInfo
+ * @property {string} Name
+ * @property {string} Filename
+ * @property {string[]} Arguments
+ * @property {string} WorkingDirectory
+ * @property {boolean} AutoRestart
+ * @property {boolean} Enabled
+ */
+
+/**
+ * @typedef {Object} RuntimeProcessInfo
+ * @property {string} Name
+ * @property {string} Filename
+ * @property {string[]} Arguments
+ * @property {string} WorkingDirectory
+ * @property {boolean} AutoRestart
+ * @property {number} ProcessId
+ * @property {number} ExitCode
+ * @property {number} RestartCount
+ */
+
+/**
+ * @typedef {Object} NewProcess
+ * @property {string} Name
+ * @property {string} Filename
+ * @property {string[]} Arguments
+ * @property {string} WorkingDirectory
+ */
+
+/**
+ * @typedef {Object} SetPropertyIpcPacket
+ * @property {string} Process
+ * @property {string} Property
+ * @property {string} data
+ */
+
+/**
+ * @typedef {Object} DaemonStatus
+ * @property {number} Processes
+ * @property {boolean} NotSaved
+ * @property {string} Directory
+ * @property {string} Version
+ */
+
+/**
+ * @typedef {Object} Config
+ * @property {boolean} LogIpc
+ * @property {boolean} FormatConfig
+ * @property {number} MaxRestarts
+ * @property {boolean} LogProcessOutput
+ */
+
+class JandIpcError extends Error {
+    constructor(...params) {
+        super(...params)
+
+        this.name = 'JandIpcError'
+    }
+}
+
+/**
+ * @private
+ * @param {string} type 
+ * @param {string | object | boolean | number} [data]
+ */
+ function sendData(type, data) {
+    if(DEBUG) console.log(`Sent ${type} ${data}`)
+    socket.write(
+        JSON.stringify({
+            Type: type,
+            Data: data
+        })
+    )
+}
+
+/**
+ * Expect a response from the IPC channel. Either a RegExp or an array of object fields if response is JSON.
+ * @private
+ * @param {RegExp | Array} match 
+ * @param {boolean} isarray | If matching obj, is it in an array? This will only match the first object.
+ */
+function expectResponse(match, isarray=false) {
+    return new Promise((resolve, reject) => {
+        socket.on('data', data_buffer => {
+            if(data_buffer.toString().startsWith('ERR')) throw new JandIpcError(data_buffer.toString())
+
+            if (DEBUG) console.log(`Received ${data_buffer.toString()}`)
+
+            if (match instanceof RegExp) {
+                
+                if (match.test(data_buffer.toString())) {
+                    resolve(data_buffer.toString())
+                }
+            
+            } else {
+                try {
+                    const data = JSON.parse(data_buffer)                    
+                    const targetFields = 
+                    isarray? Object.keys(data[0]) : Object.keys(data)
+                    for (const field of match) {
+                        if (!targetFields.find(i => i == field)) {
+                            return
+                        }
+                    }
+
+                    resolve(data)
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        })
+    }
+    )
+}
+
+/**
+ * Connect to the JanD IPC socket
+ */
+module.exports.connect = async function () {
+    socket = net.connect('/tmp/CoreFxPipe_jand')
+}
+
+/**
+ * @returns {Promise<ProcessInfo[]>}
+ */
+module.exports.getProcessList = async function () {
+    sendData('get-process-list')
+    return await expectResponse(['Name', 'Enabled'], true)
+}
+
+/**
+ * @returns {Promise<RuntimeProcessInfo[]>}
+ */
+module.exports.getRuntimeProcessList = async function () {
+    sendData('get-processes')
+    return await expectResponse(['Name', 'Running', 'Stopped'], true)
+}
+
+
+/**
+ * 
+ * @returns {Promise<DaemonStatus>}
+ */
+module.exports.getDaemonStatus = async function () {
+    sendData('status')
+    return await expectResponse(['Processes', 'NotSaved'])
+}
+
+/**
+ * 
+ * @param {String} oldname 
+ * @param {String} newname 
+ */
+module.exports.renameProcess = async function(oldname, newname) {
+    sendData('rename-process', `${oldname}:${newname}`)
+    const data = await expectResponse(/done|ERR:.+/)
+    if(data == 'done') return
+}
+
+/**
+ * Send a line to the target process. 0.7+ only.    
+ * @param {string} processname 
+ * @param {string} text 
+ * @returns 
+ */
+module.exports.sendProcessStdinLine = async function(processname, text) {
+    sendData('send-process-stdin-line', `${processname}:${text}`)
+    await expectResponse(/done/)
+    return
+}
+
+/**
+ * Exit the daemon.
+ */
+module.exports.exit = async function() {
+    sendData('exit')
+}
+
+/**
+ * Enable/disable a process
+ * @param {string} process 
+ * @param {boolean} enabled 
+ */
+module.exports.setEnabled = async function(process, enabled) {
+    sendData('set-enabled', `${process}:${enabled.toString()}`)
+    await expectResponse(/done/)
+    return
+}
+
+/**
+ * 
+ * @param {string} process 
+ * @param {string} property 
+ * @param {string} data 
+ */
+module.exports.setProcessProperty = async function(process, property, data) {
+    sendData('set-process-property', {
+        Process: process,
+        Property: property,
+        Data: data
+    })
+    await expectResponse(/done/)
+    return
+}
+
+/**
+ * 
+ * @param {ProcessInfo} process 
+ * @returns 
+ */
+module.exports.getProcessInfo = async function(process) {
+    sendData('get-process-info', process)
+    return await expectResponse(['Name', 'Filename', 'Arguments', 'WorkingDirectory', 'AutoRestart', 'Enabled'])
+}
+
+/**
+ * 
+ * @param {string} process 
+ * @returns true if process was running
+ */
+module.exports.stopProcess = async function(process) {
+    sendData('stop-process', process)
+    const response = await expectResponse(/killed|already-stopped/)
+    return response == 'killed'
+}
+
+/**
+ * @param {string} process 
+ */
+module.exports.restartProcess = async function(process) {
+    sendData('restart-process', process)
+    await expectResponse(/done/)
+    return
+}
+
+/**
+ * Starts a new process and enables it, but does not run it
+ * @param {NewProcess} process 
+ */
+module.exports.newProcess = async function(process) {
+    sendData('new-process', process)
+    const res = await expectResponse(/done|ERR:.+/)
+    if(res == 'done') return
+}
+
+module.exports.saveConfig = async function() {
+    sendData('save-config')
+    await expectResponse(/done/)
+    return
+}
+
+/**
+ * @returns {Promise<Config>}
+ */
+module.exports.getConfig = async function() {
+    sendData('get-config')
+    return await expectResponse(['LogIpc', 'FormatConfig'])
+}
+
+/**
+ * @param {string} option 
+ * @param {string} value 
+ * @returns 
+ */
+module.exports.setConfig = async function(option, value) {
+    sendData('set-config', `${option}:${value}`)
+    const res = await expectResponse(/done|Option.+/)
+    if(res == 'done') return
+}
+
+module.exports.flushAllLogs = async function() {
+    sendData('flush-all-logs')
+    await expectResponse(/done/)
+    return
+}
